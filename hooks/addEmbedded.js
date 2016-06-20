75743e5f20a40d28064344f7b60e27635c9cc168
Bryan Tafel <btafel@Bryans-Mac.local>
2016-06-20 12:33:26 -0400

    test hooks

--- hooks/addEmbedded.js
+++ hooks/addEmbedded.js
@@ -1,8 +1,8 @@
 // 'use strict';
 
-var xcode = require('xcode'),
-    fs = require('fs'),
-    path = require('path');
+var fs = require("fs");
+var path = require("path");
+var child_process = require("child_process");
 
 module.exports = function(context) {
     // if(process.length >=5 && process.argv[1].indexOf('cordova') == -1) {
@@ -12,137 +12,168 @@ module.exports = function(context) {
     // }
 
     console.log("addEmbedded", context);
-    var platforms = context.opts.cordova.platforms;
 
-    // We can bail out if the iOS platform isn't present.
-    if (platforms.indexOf("ios") === -1) {
-        deferral.resolve();
-        return deferral.promise;
-    }
+    var pluginDir = path.resolve(__dirname, "../");
 
+    // Need a promise so that the install waits for us to complete our project modifications
+    // before the plugin gets installed.
+    var Q = context.requireCordovaModule("q");
+    var deferral = new Q.defer();
 
-    function fromDir(startPath,filter, rec, multiple){
-        if (!fs.existsSync(startPath)){
-            console.log("no dir ", startPath);
-            return;
+    //BT commented -> incompatibility error with Meteor/cordova
+    // child_process.execSync("npm --prefix " + pluginDir + " install " + pluginDir);
+    child_process.exec("npm --prefix " + pluginDir + " install " + pluginDir, function(){
+        var xcode = require("xcode");
+
+        var platforms = context.opts.cordova.platforms;
+
+        // We can bail out if the iOS platform isn't present.
+        if (platforms.indexOf("ios") === -1) {
+            console.log("not ios");
+            deferral.resolve();
+            return deferral.promise;
         }
 
-        var files=fs.readdirSync(startPath);
-        var resultFiles = []
-        for(var i=0;i<files.length;i++){
-            var filename=path.join(startPath,files[i]);
-            var stat = fs.lstatSync(filename);
-            if (stat.isDirectory() && rec){
-                fromDir(filename,filter); //recurse
+
+        function fromDir(startPath,filter, rec, multiple){
+            if (!fs.existsSync(startPath)){
+                console.log("no dir ", startPath);
+                return;
             }
 
-            if (filename.indexOf(filter)>=0) {
-                if (multiple) {
-                    resultFiles.push(filename);
-                } else {
-                    return filename;
+            var files=fs.readdirSync(startPath);
+            var resultFiles = []
+            for(var i=0;i<files.length;i++){
+                var filename=path.join(startPath,files[i]);
+                var stat = fs.lstatSync(filename);
+                if (stat.isDirectory() && rec){
+                    fromDir(filename,filter); //recurse
+                }
+
+                if (filename.indexOf(filter)>=0) {
+                    if (multiple) {
+                        resultFiles.push(filename);
+                    } else {
+                        return filename;
+                    }
                 }
             }
-        }
-        if(multiple) {
-            return resultFiles;
-        }
-    }
-
-    function getFileIdAndRemoveFromFrameworks(myProj, fileBasename) {
-        var fileId = '';
-        var pbxFrameworksBuildPhaseObjFiles = myProj.pbxFrameworksBuildPhaseObj(myProj.getFirstTarget().uuid).files;
-        for(var i=0; i<pbxFrameworksBuildPhaseObjFiles.length;i++) {
-            var frameworkBuildPhaseFile = pbxFrameworksBuildPhaseObjFiles[i];
-            if(frameworkBuildPhaseFile.comment && frameworkBuildPhaseFile.comment.indexOf(fileBasename) != -1) {
-                fileId = frameworkBuildPhaseFile.value;
-                pbxFrameworksBuildPhaseObjFiles.splice(i,1); // MUST remove from frameworks build phase or else CodeSignOnCopy won't do anything.
-                break;
+            if(multiple) {
+                return resultFiles;
             }
         }
-        return fileId;
-    }
 
-    function getFileRefFromName(myProj, fName) {
-        var fileReferences = myProj.hash.project.objects['PBXFileReference'];
-        var fileRef = '';
-        for(var ref in fileReferences) {
-            if(ref.indexOf('_comment') == -1) {
-                var tmpFileRef = fileReferences[ref];
-                if(tmpFileRef.name && tmpFileRef.name.indexOf(fName) != -1) {
-                    fileRef = ref;
+        function getFileIdAndRemoveFromFrameworks(myProj, fileBasename) {
+            var fileId = '';
+            var pbxFrameworksBuildPhaseObjFiles = myProj.pbxFrameworksBuildPhaseObj(myProj.getFirstTarget().uuid).files;
+            for(var i=0; i<pbxFrameworksBuildPhaseObjFiles.length;i++) {
+                var frameworkBuildPhaseFile = pbxFrameworksBuildPhaseObjFiles[i];
+                if(frameworkBuildPhaseFile.comment && frameworkBuildPhaseFile.comment.indexOf(fileBasename) != -1) {
+                    fileId = frameworkBuildPhaseFile.value;
+                    pbxFrameworksBuildPhaseObjFiles.splice(i,1); // MUST remove from frameworks build phase or else CodeSignOnCopy won't do anything.
                     break;
                 }
             }
+            return fileId;
         }
-        return fileRef;
-    }
 
-    var xcodeProjPath = fromDir('platforms/ios','.xcodeproj', false);
-    var projectPath = xcodeProjPath + '/project.pbxproj';
-    var myProj = xcode.project(projectPath);
+        function getFileRefFromName(myProj, fName) {
+            var fileReferences = myProj.hash.project.objects['PBXFileReference'];
+            var fileRef = '';
+            for(var ref in fileReferences) {
+                if(ref.indexOf('_comment') == -1) {
+                    var tmpFileRef = fileReferences[ref];
+                    if(tmpFileRef.name && tmpFileRef.name.indexOf(fName) != -1) {
+                        fileRef = ref;
+                        break;
+                    }
+                }
+            }
+            return fileRef;
+        }
 
-    function addRunpathSearchBuildProperty(proj, build) {
-       var LD_RUNPATH_SEARCH_PATHS =  proj.getBuildProperty("LD_RUNPATH_SEARCH_PATHS", build);
-       if(!LD_RUNPATH_SEARCH_PATHS) {
-          proj.addBuildProperty("LD_RUNPATH_SEARCH_PATHS", "\"$(inherited) @executable_path/Frameworks\"", build);
-       } else if(LD_RUNPATH_SEARCH_PATHS.indexOf("@executable_path/Frameworks") == -1) {
-          var newValue = LD_RUNPATH_SEARCH_PATHS.substr(0,LD_RUNPATH_SEARCH_PATHS.length-1);
-          newValue += ' @executable_path/Frameworks\"';
-          proj.updateBuildProperty("LD_RUNPATH_SEARCH_PATHS", newValue, build);
-       }
-    }
+        var xcodeProjPath = fromDir('platforms/ios','.xcodeproj', false);
+        var projectPath = xcodeProjPath + '/project.pbxproj';
+        var myProj = xcode.project(projectPath);
 
-    myProj.parseSync();
-    addRunpathSearchBuildProperty(myProj, "Debug");
-    addRunpathSearchBuildProperty(myProj, "Release");
+        function addRunpathSearchBuildProperty(proj, build) {
+           var LD_RUNPATH_SEARCH_PATHS =  proj.getBuildProperty("LD_RUNPATH_SEARCH_PATHS", build);
+           if(!LD_RUNPATH_SEARCH_PATHS) {
+              proj.addBuildProperty("LD_RUNPATH_SEARCH_PATHS", "\"$(inherited) @executable_path/Frameworks\"", build);
+           } else if(LD_RUNPATH_SEARCH_PATHS.indexOf("@executable_path/Frameworks") == -1) {
+              var newValue = LD_RUNPATH_SEARCH_PATHS.substr(0,LD_RUNPATH_SEARCH_PATHS.length-1);
+              newValue += ' @executable_path/Frameworks\"';
+              proj.updateBuildProperty("LD_RUNPATH_SEARCH_PATHS", newValue, build);
+           }
+        }
 
-    // unquote (remove trailing ")
-    var projectName = myProj.getFirstTarget().firstTarget.name.substr(1);
-    projectName = projectName.substr(0, projectName.length-1); //Removing the char " at beginning and the end.
+        // myProj.parse(function(err) {
+        myProj.parseSync();
+          addRunpathSearchBuildProperty(myProj, "Debug");
+          addRunpathSearchBuildProperty(myProj, "Release");
 
-    var groupName = 'Embed Frameworks ' + context.opts.plugin.id;
-    var pluginPathInPlatformIosDir = projectName + '/Plugins/' + context.opts.plugin.id;
+          // unquote (remove trailing ")
+          var projectName = myProj.getFirstTarget().firstTarget.name.substr(1);
+          projectName = projectName.substr(0, projectName.length-1); //Removing the char " at beginning and the end.
 
-    process.chdir('./platforms/ios');
-    var frameworkFilesToEmbed = fromDir(pluginPathInPlatformIosDir ,'.framework', false, true);
-    process.chdir('../../');
+          var groupName = 'Embed Frameworks ' + context.opts.plugin.id;
+          var pluginPathInPlatformIosDir = projectName + '/Plugins/' + context.opts.plugin.id;
 
-    if(!frameworkFilesToEmbed.length) return;
+          process.chdir('./platforms/ios');
+          var frameworkFilesToEmbed = fromDir(pluginPathInPlatformIosDir ,'.framework', false, true);
+          process.chdir('../../');
 
-    myProj.addBuildPhase(frameworkFilesToEmbed, 'PBXCopyFilesBuildPhase', groupName, myProj.getFirstTarget().uuid, 'frameworks');
+          console.log("frameworkFilesToEmbed", frameworkFilesToEmbed);
+          if(!frameworkFilesToEmbed.length) return;
 
-    for(var frmFileFullPath in frameworkFilesToEmbed) {
-        var justFrameworkFile = path.basename(frmFileFullPath);
-        var fileRef = getFileRefFromName(myProj, justFrameworkFile);
-        var fileId = getFileIdAndRemoveFromFrameworks(myProj, justFrameworkFile);
+          myProj.addBuildPhase(frameworkFilesToEmbed, 'PBXCopyFilesBuildPhase', groupName, myProj.getFirstTarget().uuid, 'frameworks');
 
-        // Adding PBXBuildFile for embedded frameworks
-        var file = {
-            uuid: fileId,
-            basename: justFrameworkFile,
-            settings: {
-                ATTRIBUTES: ["CodeSignOnCopy", "RemoveHeadersOnCopy"]
-            },
+          // for(var frmFileFullPath in frameworkFilesToEmbed) {
+          frameworkFilesToEmbed.forEach(function(frmFileFullPath){
+          // for(var i=frameworkFilesToEmbed.length-1; i>=0; i--){ 
+              // var frmFileFullPath = frameworkFilesToEmbed[i];
 
-            fileRef:fileRef,
-            group:groupName
-        };
-        myProj.addToPbxBuildFileSection(file);
+              console.log("frmFileFullPath", frmFileFullPath);
+              var justFrameworkFile = path.basename(frmFileFullPath);
+              var fileRef = getFileRefFromName(myProj, justFrameworkFile);
+              var fileId = getFileIdAndRemoveFromFrameworks(myProj, justFrameworkFile);
+
+              // Adding PBXBuildFile for embedded frameworks
+              var file = {
+                  uuid: fileId,
+                  basename: justFrameworkFile,
+                  settings: {
+                      ATTRIBUTES: ["CodeSignOnCopy", "RemoveHeadersOnCopy"]
+                  },
+
+                  fileRef:fileRef,
+                  group:groupName
+              };
+              myProj.addToPbxBuildFileSection(file);
 
 
-        // Adding to Frameworks as well (separate PBXBuildFile)
-        var newFrameworkFileEntry = {
-            uuid: myProj.generateUuid(),
-            basename: justFrameworkFile,
+              // Adding to Frameworks as well (separate PBXBuildFile)
+              var newFrameworkFileEntry = {
+                  uuid: myProj.generateUuid(),
+                  basename: justFrameworkFile,
 
-            fileRef:fileRef,
-            group: "Frameworks"
-        };
-        myProj.addToPbxBuildFileSection(newFrameworkFileEntry);
-        myProj.addToPbxFrameworksBuildPhase(newFrameworkFileEntry);
-    }
+                  fileRef:fileRef,
+                  group: "Frameworks"
+              };
+              myProj.addToPbxBuildFileSection(newFrameworkFileEntry);
+              myProj.addToPbxFrameworksBuildPhase(newFrameworkFileEntry);
+          });
+          // }
 
-    fs.writeFileSync(projectPath, myProj.writeSync());
-    console.log('Embedded Frameworks In ' + context.opts.plugin.id);
+          fs.writeFileSync(projectPath, myProj.writeSync());
+          console.log("projectPath", projectPath)
+          console.log('Embedded Frameworks In ' + context.opts.plugin.id);
+          deferral.resolve();
+
+        // });
+
+    });
+
+    return deferral.promise;
+
 };
--- hooks/after_build.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(context) {
-
-  console.log("after_build", context);
-}
